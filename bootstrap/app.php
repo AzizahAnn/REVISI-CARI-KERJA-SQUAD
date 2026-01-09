75a17627ed3314caf1a666bcd8855de27e20cd45
AzizahAnn <aninuri658@student.uns.ac.id>
2026-01-09 15:04:29 +0700

    paste repo CARI-KERJA-SQUAD

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'perusahaan' => \App\Http\Middleware\PerusahaanMiddleware::class,
+            'pelamar' => \App\Http\Middleware\PelamarMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
